ac8a0a8742cfacdf4086954f5a2f2e615461f2ca
Roni Laukkarinen <roni@laukkarinen.info>
2014-12-30 21:07:13 +0200

    cache

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@ set :application, "pulina"
 set :repo_url,  "git@bitbucket.org:ronilaukkarinen/pulina.git"
 set :branch, :master
 set :log_level, :debug
-set :linked_files, %w{.env}
+set :linked_files, %w{.env content/advanced-cache.php content/wp-cache-config.php}
 set :linked_dirs, %w{content/uploads}
 set :composer_install_flags, '--no-dev --prefer-dist --no-scripts --optimize-autoloader'
 set :composer_roles, :all
