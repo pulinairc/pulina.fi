ca4d0b54f07d4c92ec8100ebe311cde0b0ff58d9
Roni Laukkarinen <roni@laukkarinen.info>
2015-05-10 15:18:02 +0300

    WP updates

--- config/application.php
+++ config/application.php
@@ -56,8 +56,8 @@ define('NONCE_SALT',       getenv('NONCE_SALT'));
 /**
  * Custom Settings
  */
-define('AUTOMATIC_UPDATER_DISABLED', true);
-define('DISABLE_WP_CRON', true);
+define('AUTOMATIC_UPDATER_DISABLED', false);
+define('DISABLE_WP_CRON', false);
 define('DISALLOW_FILE_EDIT', true);
 
 /**
