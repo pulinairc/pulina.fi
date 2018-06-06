d1e690ade88d611558ba3e7ce157bc48b758fea2
Roni Laukkarinen <roni@laukkarinen.info>
2018-06-06 15:10:33 +0300

    Remove cache from telegram

--- content/themes/light/template-parts/telegram.php
+++ content/themes/light/template-parts/telegram.php
@@ -7,30 +7,10 @@
  * @package light
  */
 
-$telegram_bot_api_url = 'https://api.telegram.org/' . getenv('TELEGRAM_BOTTOKEN') . '/getUpdates?offset=-1&limit=1';
+$telegram_bot_api_url = file_get_contents( 'https://api.telegram.org/' . getenv( 'TELEGRAM_BOTTOKEN' ) . '/getUpdates?offset=-1&limit=1' );
 
-// Fetch data and set up simple cache
-$telegram_cachefile = get_theme_file_path( 'inc/cache/telegram.json' );
-$telegram_cachetime = 60; // 1 minute
+$data = json_decode( $telegram_bot_api_url, true );
 
-// If cache file does not exist, let's create it
-if ( ! file_exists( $telegram_cachefile ) ) {
-  touch( $telegram_cachefile );
-  copy( $telegram_bot_api_url, $telegram_cachefile );
-}
-
-// Set up feed
-$json = file_get_contents( $telegram_cachefile );
-$data = json_decode( $json, true );
-
-// If file is older than cache time, overwrite file
-if ( time() - filemtime( $telegram_cachefile ) > 2 * $telegram_cachetime ) {
-  copy( $telegram_bot_api_url, $telegram_cachefile );
-}
-
-// Do stuff
-foreach ( array_reverse($data['result']) as $result ) :
-  // Test data:
-  // echo var_dump($result['message']['date']) . '<br /><br />';
+foreach ( array_reverse( $data['result'] ) as $result ) :
   echo time2str( $result['message']['date'] );
 endforeach;
