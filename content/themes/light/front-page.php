e3ed6d21e02cdf00ed6f9557d010b830804ad5c5
Roni Laukkarinen <roni@dude.fi>
2020-04-22 09:08:25 +0300

    Remove duplicate

--- content/themes/light/front-page.php
+++ content/themes/light/front-page.php
@@ -103,35 +103,6 @@ get_header(); ?>
       endif; ?>
     </div>
 
-    <div id="placement">
-      <?php $args = array(
-        'post_type' => 'ads',
-        'posts_per_page' => 1,
-        'no_found_rows' => true,
-        'post_status' => 'publish',
-      );
-
-      $ad = new WP_Query( $args );
-      if ( $ad->have_posts() ) :
-        while ( $ad->have_posts() ) :
-          $ad->the_post();
-
-          if ( 'etusivu' === get_field( 'slotti' ) ) :
-            if ( strtotime( get_field( 'eraantymispaiva' ) ) > time() ) : ?>
-
-              <div class="advertisement ad advert textad">
-                <div class="ad-top ad textad">
-                  <?php echo get_field( 'mainoskoodi' ); // WPCS: XSS OK. ?>
-                </div><!-- .textad -->
-              </div><!-- .ad -->
-
-              <?php
-            endif;
-          endif;
-        endwhile;
-      endif; ?>
-    </div>
-
 	</div>
 
 </div><!-- .slide-esittely -->
