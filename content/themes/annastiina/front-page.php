887a487a1318f77579c79010313d4af201f1997a
Roni Laukkarinen <roni@dude.fi>
2021-02-18 14:04:26 +0200

    Init hall of fame, toptod

--- content/themes/annastiina/front-page.php
+++ content/themes/annastiina/front-page.php
@@ -23,7 +23,6 @@ get_header(); ?>
 <main class="site-main">
 
 <section class="block block-hero block-hero-fp">
-
   <div class="container">
     <div class="cols">
       <div class="col col-content">
@@ -41,6 +40,59 @@ get_header(); ?>
         </div>
       </div>
     </div>
+  </div>
+</section>
+
+<section class="block block-hall-of-fame">
+  <div class="container">
+
+    <!-- Toptod markup here, TODO: Add correct screen-reader-text markup to peikko.us/toptod.php -->
+    <h2>!toptod, eli päivän aktiivisimmat</h2>
+
+      <ol><li>
+          <div class="points">
+            <div class="bar"><div class="progress progress-120" style="width: 120%;"><b>mustikkasoppa</b> <span class="value">2400</span></div></div>
+          </div>
+          </li><li>
+          <div class="points">
+            <div class="bar"><div class="progress progress-50" style="width: 45.2%;"><b>Nefastos</b> <span class="value">904</span></div></div>
+          </div>
+          </li><li>
+          <div class="points">
+            <div class="bar"><div class="progress progress-50" style="width: 43.1%;"><b>ivy_</b> <span class="value">862</span></div></div>
+          </div>
+          </li><li>
+          <div class="points">
+            <div class="bar"><div class="progress progress-40" style="width: 33.55%;"><b>kummitus</b> <span class="value">671</span></div></div>
+          </div>
+          </li><li>
+          <div class="points">
+            <div class="bar"><div class="progress progress-30" style="width: 25.8%;"><b>rolle</b> <span class="value">516</span></div></div>
+          </div>
+          </li><li>
+          <div class="points">
+            <div class="bar"><div class="progress progress-20" style="width: 18.15%;"><b>lapyo</b> <span class="value">363</span></div></div>
+          </div>
+          </li><li>
+          <div class="points">
+            <div class="bar"><div class="progress progress-20" style="width: 13%;"><b>hendrix</b> <span class="value">260</span></div></div>
+          </div>
+          </li><li>
+          <div class="points">
+            <div class="bar"><div class="progress progress-20" style="width: 12.2%;"><b>nmr</b> <span class="value">244</span></div></div>
+          </div>
+          </li><li>
+          <div class="points">
+            <div class="bar"><div class="progress progress-20" style="width: 11.35%;"><b>R1NN1</b> <span class="value">227</span></div></div>
+          </div>
+          </li><li>
+          <div class="points">
+            <div class="bar"><div class="progress progress-10" style="width: 7.6%;"><b>LM-</b> <span class="value">152</span></div></div>
+          </div>
+          </li></ol>
+    </div>
+    <!-- Toptod markup ends -->
+
   </div>
 </section>
 
