66a8f53fd234d311325e25402f6ad55c54ad3417
Roni Laukkarinen <roni@dude.fi>
2021-02-22 16:12:59 +0200

    Update cert

--- gulp/config.js
+++ gulp/config.js
@@ -46,8 +46,8 @@ module.exports = {
       },
       // Generate with: mkdir -p /var/www/certs && cd /var/www/certs && mkcert localhost 192.168.x.xxx ::1
       https: {
-        key: "/var/www/certs/localhost+1-key.pem",
-        cert: "/var/www/certs/localhost+1.pem",
+        key: "/var/www/certs/localhost+2-key.pem",
+        cert: "/var/www/certs/localhost+2.pem",
       }
     },
     watch: [
