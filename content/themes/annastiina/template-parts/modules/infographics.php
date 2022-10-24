0c1a6db716f721362fc74688dc1b241db9dd3265
Roni Laukkarinen <roni@dude.fi>
2022-10-24 14:55:17 +0300

    Add some extra check so that front page would not break

--- content/themes/annastiina/template-parts/modules/infographics.php
+++ content/themes/annastiina/template-parts/modules/infographics.php
@@ -7,6 +7,23 @@
 
 namespace Air_Light;
 
+require_once get_theme_file_path( 'inc/includes/simplehtmldom_1_9_1/simple_html_dom.php' );
+
+if (
+  filesize( get_theme_file_path( 'inc/cache/numbers.html' ) ) === 0 ||
+  filesize( get_theme_file_path( 'inc/cache/peak.db' ) ) === 0 ||
+  filesize( get_theme_file_path( 'inc/cache/stats.html' ) ) === 0
+  ) {
+    return;
+}
+
+if (
+    ! file_get_html( 'https://botit.pulina.fi/pulina.html' ) ||
+    ! file_get_html( 'https://botit.pulina.fi/peak.db' ) ||
+    ! file_get_html( 'https://www.pulina.fi/statsit/index.html' )
+  ) {
+  return;
+}
 ?>
 
 <section class="block block-infographics">
